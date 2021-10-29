b132c29b3db04ba5208e535072e59baacdff21e0
yakuznecov <yakuznecov@mail.ru>
2021-10-29 09:43:50 +0300

    add destroy

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-const modal = $.modal();
+const modal = $.modal({
+	title: 'New Modal',
+	closable: true,
+	content: `
+        <h4>Modal is working</h4>
+        <p>Lorem ipsum dolor sit amet consectetur</p>
+    `,
+	width: '400px',
+});
--- plugins/modal.js
+++ plugins/modal.js
@@ -1,19 +1,19 @@
 function _createModal(options) {
+	const DEFAULT_WIDTH = '600px'; // значение по умолчанию, если ширина не передавалась
 	const modal = document.createElement('div');
 	modal.classList.add('vmodal');
 	modal.insertAdjacentHTML(
 		'afterbegin',
 		`
     
-        <div class="modal-overlay">
-            <div class="modal-window">
+        <div class="modal-overlay" data-close='true'>
+            <div class="modal-window" style='width: ${options.width || DEFAULT_WIDTH}'>
                 <div class="modal-header">
-                    <span class="modal-title">Modal title</span>
-                    <span class="modal-close">&times;</span>
+                    <span class="modal-title">${options.title || 'Окно'}</span>
+                    ${options.closable ? `<span class="modal-close" data-close='true'>&times;</span>` : ''} 
                 </div>
                 <div class="modal-body">
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    ${options.content || ''}
                 </div>
                 <div class="modal-footer">
                     <button>Ok</button>
@@ -33,8 +33,9 @@ $.modal = function (options) {
 	const ANIMATION_SPEED = 200;
 	const $modal = _createModal(options);
 	let closing = false;
-	// возврат методов
-	return {
+	let destroyed = false;
+
+	const modal = {
 		open() {
 			!closing && $modal.classList.add('open'); // Добавляем класс для открытия
 		},
@@ -47,6 +48,21 @@ $.modal = function (options) {
 				closing = false;
 			}, ANIMATION_SPEED);
 		},
-		destroy() {},
 	};
+
+	const listener = (event) => {
+		if (event.target.dataset.close) {
+			modal.close();
+		}
+	};
+
+	$modal.addEventListener('click', listener);
+	// возврат методов
+	return Object.assign(modal, {
+		destroy() {
+			$modal.parentNode.removeChild($modal);
+			$modal.removeEventListener('click', listener);
+			destroyed = true;
+		},
+	});
 };
